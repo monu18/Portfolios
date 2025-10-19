667dace66561cbf05967b6735acbb545fdb8c9cc
monu18 <mozis.bot@gmail.com>
2025-10-19 15:00:51 -0500

    removed useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 import portfolioData from "@/utils/portfolioData";
 // import Chip from "@/components/Chip";
